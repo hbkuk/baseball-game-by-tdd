a0ac71ae1a46470b57eb83759ffeba3b6bd17919
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-10-20 11:13:06 +0900

    refactor: 테스트 코드 리팩토링

--- src/test/java/baseBall/GameManagerTest.java
+++ src/test/java/baseBall/GameManagerTest.java
@@ -1,60 +1,70 @@
 package baseBall;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GameManagerTest {
 
+    OutputStream out;
+
+    @BeforeEach
+    void setUp() {
+        out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+    }
+
     @Test
+    @DisplayName("정상적인 숫자로 구성된 문자열을 받을 때까지 특정 출력을 반복하고, 정상적인 숫자로 구성된 해당 문자열을 반환한다.")
     void getInput() {
-        String input = "abs" + System.lineSeparator() + "123" + System.lineSeparator();
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
+        setInputStreamInSystem("abs", "123");
 
         GameManager gameManager = new GameManager(new Scanner(System.in));
+
         assertEquals("123", gameManager.getInput());
-
-        String expectedOutput = String.format("숫자를 입력해 주세요 : %n숫자를 입력해 주세요 : %n");
-        assertEquals(expectedOutput, out.toString());
+        assertExpectedOutput("숫자를 입력해 주세요 : %n숫자를 입력해 주세요 : %n");
     }
 
-    @Test
-    void askUserForRestart() {
-        String input = "5" + System.lineSeparator() + "1" + System.lineSeparator();
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
+    @DisplayName("숫자로 구성된 하나의 문자열(1 혹은 2)를 받을 때까지 특정 출력을 반복하고, 1이라면 true, 2라면 false 반환한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:false"}, delimiter = ':')
+    void askUserForRestart_true(String input, boolean expected) {
+        setInputStreamInSystem("5", input);
 
         GameManager gameManager = new GameManager(new Scanner(System.in));
-        assertEquals(true, gameManager.askUserForRestart());
 
-        String expectedOutput = String.format("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.%n다시 제대로 숫자를 입력해 주세요 : %n");
-        assertEquals(expectedOutput, out.toString());
+        assertEquals(expected, gameManager.askUserForRestart());
+        assertExpectedOutput("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.%n다시 제대로 숫자를 입력해 주세요 : %n");
     }
 
+    @DisplayName("숫자로 구성된 값을 전달하면 결과를 출력한다.")
     @Test
     void playSingleGame() {
-        String input = "312" + System.lineSeparator() + "123" + System.lineSeparator();
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
+        setInputStreamInSystem("312", "123");
 
         GameManager gameManager = new GameManager(new Scanner(System.in));
         gameManager.playSingleGame(new Balls(Arrays.asList(1, 2, 3)));
 
         assertThat(out.toString()).contains("Ball 3", "All Strike!!!");
+    }
+
+    private void assertExpectedOutput(String format) {
+        String expectedOutput = String.format(format);
+        assertEquals(expectedOutput, out.toString());
+    }
+
+    private void setInputStreamInSystem(String abs, String number) {
+        String input = abs + System.lineSeparator() + number + System.lineSeparator();
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
     }
 }
